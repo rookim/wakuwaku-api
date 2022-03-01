6ab7c7636c44e7546c39a51a67dd3c2c4e1dfe88
ro <kronew6@gmail.com>
2022-02-28 20:59:15 -0600

    adds data to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user = User.create!([
+  {email: "helloherokim@gmail.com", username: "Ro", phone_number: "2246003311", password: "password"},
+  {email: "cohk21@gmail.com", username: "cohk21", password: "password"}
+])
+
+favorite = Favorite.create!([
+  {user_id: User.first.id, tvmaze_id: 919},
+  {user_id: User.last.id, tvmaze_id: 59412}
+])
