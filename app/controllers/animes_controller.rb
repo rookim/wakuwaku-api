19dcfdb4851f53379e4d5b5f607c7a29bb2873dd
ro <kronew6@gmail.com>
2022-03-03 21:50:50 -0600

    adds logic to check if anime is in user's favorites

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -1,19 +1,25 @@
 class AnimesController < ApplicationController
 
   def index
-    # maybe only allow search for currently airing anime?
+    # maybe only allow search for currently airing anime? LOOK AT API
     response = HTTP.get("https://api.tvmaze.com/search/shows?q=#{params[:title]}")
 
     # for now, raw body data; can parse later
     animes = response.parse(:json)
+    if current_user
+      animes.each do |anime|
+        anime[:favorited] = Favorite.exists?(user_id: current_user.id, tvmaze_id: anime["show"]["id"])
+      end  
+    end
+
     render json: animes
   end
 
-  def show
-    response = HTTP.get("https://api.tvmaze.com/shows/#{params[:id]}")
+  # def show
+  #   response = HTTP.get("https://api.tvmaze.com/shows/#{params[:id]}")
     
-    # for now, raw body data; can parse later
-    render json: response.parse(:json)
-  end
+  #   # for now, raw body data; can parse later
+  #   render json: response.parse(:json)
+  # end
 
 end
