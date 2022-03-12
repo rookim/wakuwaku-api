3242f2d33b13002533721ee4a7815d05a62efa5b
ro <kronew6@gmail.com>
2022-03-11 21:43:13 -0600

    adds logic to grab information about anime episodes

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -12,8 +12,21 @@ class AnimesController < ApplicationController
         animes << show
       end
     end
+    
+    # add airtime and airdate for each anime
+    if animes
+      animes.each do |anime|
+        ep_link = anime["show"]["_links"]["nextepisode"]["href"]
+        response2 = HTTP.get(ep_link)
+        next_ep_info = response2.parse(:json)
+  
+        anime[:next_ep] = next_ep_info
+      end
+    end
+
     if current_user
       animes.each do |anime|
+        # setting a favorited key for every anime shown (and sending to frontend anime objects that all have this key value pair)
         anime[:favorited] = Favorite.exists?(user_id: current_user.id, tvmaze_id: anime["show"]["id"])
         if anime[:favorited] == true
           id = Favorite.find_by(user_id: current_user.id, tvmaze_id: anime["show"]["id"])
@@ -25,11 +38,11 @@ class AnimesController < ApplicationController
     render json: animes
   end
   
-  def show
-    response = HTTP.get("https://api.tvmaze.com/shows/#{params[:id]}")
+  # def show
+  #   anime = HTTP.get("https://api.tvmaze.com/shows/#{params[:id]}")
     
-    # for now, raw body data; can parse later
-    render json: response.parse(:json)
-  end
+  #   # for now, raw body data; can parse later
+  #   render json: anime.parse(:json)
+  # end
 
 end
