f54c2282aa7b46ca8ca5c10fd0eaddb7ec65f1db
ro <kronew6@gmail.com>
2022-03-15 00:40:19 -0500

    adds custom password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ApplicationRecord
+  PASSWORD_FORMAT = /\A
+  (?=.{6,})          # Must contain 6 or more characters
+  (?=.*\d)           # Must contain a digit
+  (?=.*[a-z])        # Must contain a lower case character
+  (?=.*[A-Z])        # Must contain an upper case character
+  (?=.*[!?@$])       # Must contain a symbol
+/x
+  
   has_many :favorites
   has_secure_password
+
   validates :email, presence: true, uniqueness: true
-  validates :username, presence: true
+  validates :username, presence: true, length: {minimum: 3}
+  validates :password, length: {minimum: 6}, format: {with: PASSWORD_FORMAT, message: "needs to contain all the requirements!"}
 end
